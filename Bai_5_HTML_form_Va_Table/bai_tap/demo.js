f749ac58ec8f1756f4df6a473d0c31e9042acbe7
manhhung1509 <hung2332nguyenmanh@gmail.com>
2021-10-10 18:42:19 +0700

    upbai

---
+++ Bai_5_HTML_form_Va_Table/bai_tap/demo.js
@@ -0,0 +1,14 @@
+var i = 10;
+var f = 20.5
+var b = true
+var s = 'Hà Nội'
+
+document.write('i = ' + i)
+document.write('<br/>')
+document.write('f = ' + f)
+document.write('<br/>')
+document.write('b = ' + b)
+document.write('<br/>')
+document.write('s = ' + s)
+console.log(i);
+console.log(10);
